d31b445cde0d4fe49684504a52723a024b4b9ece
Hobs Towler <towlerj@oregonstate.edu>
2022-07-31 01:04:16 -0400

    game implementation

--- server.py
+++ server.py
@@ -2,26 +2,30 @@
 # GitHub username: hobstowler
 # Date: 6/22/2022
 # Description: A script for a small web server that sends a message when a user connects to it.
-
+import random
 import socket
 
 # Code Adapted from examples and documenation at:
-# https://docs.python.org/3/library/io.html#io.TextIOWrapper
 # https://docs.python.org/3/library/socket.html
-# on 6/22/2022
+# date: 7/31/2022
 
-# Requests pulled from project instructions at:
-# https://canvas.oregonstate.edu/courses/1878268/assignments/8929461?module_item_id=22337858
-# on 6/22/2022
+# Word list pulled from:
+# https://7esl.com/6-letter-words/
+# date: 7/31/2022
 
 
 class Server:
     _conn_ask = False
     _host = '127.0.0.1'
     _port = 9508
-    _game_mode = False
     _client_handle = 'SERVER >>'
     _server_handle = 'CLIENT >>'
+
+    _game_mode = False
+    _game_word = ''
+    _guesses = 5
+    _num_guesses = 0
+
 
     def __init__(self):
         pass
@@ -57,30 +61,84 @@ class Server:
             print(self._client_handle, message)
             if message == 'yes':
                 self._game_mode = True
-                reply = 'Alright, let\'s play!'
+                self.start_game()
+                reply = f'Alright, let\'s play Wordle!\r\nI\'ll pick a 6-letter word and give you {self._guesses} ' \
+                        f'guesses to get it right\r\nIf you get the right letter and position, I\'ll denote that with' \
+                        f' "$"\r\nIf you get the right letter, but the wrong position, I\'ll denote that with a "!"' \
+                        f'\r\nSo, Give me your first guess...'
                 conn.send(reply.encode())
                 print(self._server_handle, reply)
             else:
-                reply = 'Ok, maybe next time...'
+                reply = 'Ok, maybe next time...\r\nIf you want to play, just say "/game"'
                 conn.send(reply.encode())
                 print(self._server_handle, reply)
 
             while message != '/q':
                 message = conn.recv(4096).decode()
-                print('Client >>', message)
+                print(self._client_handle, message)
                 if self._game_mode:
-                    pass
+                    if message == '/q':
+                        reply = 'Okay! If you want to play again, just say "/game". If you want to quit fully, just ' \
+                                'say "/q" one more time.'
+                    else:
+                        reply = self.parseResponse(message)
                 else:
                     if message == '/q':
                         reply = 'Goodbye!'
-                        conn.send(reply.encode())
-                        print(self._server_handle, reply)
                     else:
                         reply = input(self._server_handle + ' ')
-                        conn.send(reply.encode())
+                conn.send(reply.encode())
+                print(self._server_handle, reply)
 
             serv_socket.close()
             break
+
+    def start_game(self):
+        words = []
+        with open('words') as file:
+            for line in file:
+                words.append(line.strip())
+        self._game_word = random.choice(words).lower()
+        print(self._game_word)
+
+    def parseResponse(self, message):
+        message = message.strip()
+        if len(message) < 6:
+            return 'Word is too short. Try again.'
+        if len(message) > 25:
+            return 'Word is waaaaaaaaay too long. Try again.'
+        if len(message) > 6:
+            return 'Word is too long. Try again.'
+
+        self._num_guesses += 1
+        message = message.lower()
+        if message == self._game_word:
+            return f'You got the word in {self._num_guesses} guess{"es" if self._num_guesses > 1 else ""}! {self._game_word}'
+
+
+        answer = f'You guessed:\r\n{message}\r\n'
+        guess_score = 0
+        for i in range(len(message)):
+            if message[i] == self._game_word[i]:
+                answer += '$'
+                guess_score += 2
+            elif message[i] in self._game_word:
+                answer += '!'
+                guess_score += 1
+            else:
+                answer += '_'
+        if guess_score >= 10:
+            answer += f' Wow! You\'re getting close!'
+        elif guess_score >= 7:
+            answer += f' That was a pretty good guess.'
+        elif guess_score >= 3:
+            answer += f' Keep going, you\'ll get there.'
+        remaining = self._guesses - self._num_guesses
+        guesses = f' {remaining} guess{"es" if self._num_guesses > 1 else ""} left'
+        answer += guesses
+
+        return answer
+
 
 
 if __name__ == '__main__':
